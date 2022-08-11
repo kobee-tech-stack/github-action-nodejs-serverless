d594c6fcb100e9d6bd51ed1493ab1558c7f433e6
haithai91 <78775708+haithai91@users.noreply.github.com>
2022-08-11 20:51:58 +0700

    Fix build error

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import {APIGatewayEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
-import * as os from 'os';
 
 /**
  * Follow the guide https://docs.aws.amazon.com/lambda/latest/dg/typescript-handler.html
@@ -11,15 +10,6 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        osName: os.hostname(),
-        osPlatform: os.platform(),
-        osCPU: os.cpus(),
-        osMemory: os.totalmem() / 1048576, // 1mb = 1048576
-      },
-      null,
-      2
-    ),
+    body: 'Hello World',
   };
 };
